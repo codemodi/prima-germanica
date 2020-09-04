40bb7ead4fdd04c5109168521a58d15ce704f398
pcampos119104 <pcampos1191041@gmail.com>
2020-09-04 10:49:27 -0300

    Redis set up and business logic.
    close #6

--- primagermanica/base/views.py
+++ primagermanica/base/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 import requests
+from django.core.cache import cache
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -7,7 +8,6 @@ from primagermanica.base.models import Transaction
 
 
 class ArtistView(APIView):
-    # serializer_class = serializers.HelloSerializer
 
     def get(self, request):
         cache_qs = request.GET.get('cache')
@@ -16,23 +16,20 @@ class ArtistView(APIView):
             payload = search_songs(name_qs)
             return Response(payload)
 
-        payload = get_from_redis(name_qs)
+        payload = cache.get(name_qs)
         if not payload:
             payload = search_songs(name_qs)
+            cache.set(name_qs, payload)
 
         return Response(payload)
 
 
 # todo treat the status code, can't just pass the same.
 def search_songs(name: str):
-    # todo extract to env
+    # todo extract to .env
     ACCESS_TOKEN = 'C4j5H3PjWLdZmwtj1caAIFjcAj_IXpUlwOJyo9ZI9GW9P_KGaetVnlwCkuqszLMq'
     r = requests.get('https://api.genius.com/search', params={'q': name},
                      headers={'Authorization': f'Bearer {ACCESS_TOKEN}'})
     t = Transaction(name)
     t.save()
     return r.json()
-
-
-def get_from_redis(name):
-    return search_songs(name)
