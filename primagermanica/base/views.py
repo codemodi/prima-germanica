d1854c64a55e73ac555a8c0756bcc5cb206791f8
pcampos119104 <pcampos1191041@gmail.com>
2020-09-04 07:41:52 -0300

    Dynamodb set up locally with docker. Saving transaction working.
    App doesn't access dynamodb-local inside docker network, because of a boto check on url, app must be run outside docker. open issue.
    
    close #6

--- primagermanica/base/views.py
+++ primagermanica/base/views.py
@@ -3,6 +3,8 @@ import requests
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from primagermanica.base.models import Transaction
+
 
 class ArtistView(APIView):
     # serializer_class = serializers.HelloSerializer
@@ -10,8 +12,13 @@ class ArtistView(APIView):
     def get(self, request):
         cache_qs = request.GET.get('cache')
         name_qs = request.GET.get('q')
-        cache = not (cache_qs and cache_qs.lower() == 'false')
-        payload = search_songs(name_qs)
+        if cache_qs and cache_qs.lower() == 'false':
+            payload = search_songs(name_qs)
+            return Response(payload)
+
+        payload = get_from_redis(name_qs)
+        if not payload:
+            payload = search_songs(name_qs)
 
         return Response(payload)
 
@@ -22,4 +29,10 @@ def search_songs(name: str):
     ACCESS_TOKEN = 'C4j5H3PjWLdZmwtj1caAIFjcAj_IXpUlwOJyo9ZI9GW9P_KGaetVnlwCkuqszLMq'
     r = requests.get('https://api.genius.com/search', params={'q': name},
                      headers={'Authorization': f'Bearer {ACCESS_TOKEN}'})
+    t = Transaction(name)
+    t.save()
     return r.json()
+
+
+def get_from_redis(name):
+    return search_songs(name)
